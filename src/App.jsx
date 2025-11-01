4628dedc2d655eb0c1a3fd5c3df9c06263b0049f
Murad Hasan <muradhasanshuvo00@gmail.com>
2025-11-01 20:09:17 +0600

    add banner section

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,6 @@
 import './App.css'
+import BannerSlider from './components/banner part/Banner'
+import Banner from './components/banner part/Banner'
 import Discount from './components/Discount/Discount'
 import Navbar from './components/navbar/Navbar'
 
@@ -8,6 +10,7 @@ function App() {
     <>
     <Discount/>
     <Navbar/>
+    <BannerSlider/>
     </>
   )
 }
